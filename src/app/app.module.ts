15551b1dc89b0f6ad4edb414c5e6516c36ad9b26
Depickere Sven <sven.depickere@student.howest.be>
2020-02-15 18:48:07 +0100

    Update init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
 import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppContainer } from "./container/app/app.container";
 
 @NgModule({
     bootstrap: [
-        AppComponent
+        AppContainer
     ],
     imports: [
         NativeScriptModule,
         AppRoutingModule
     ],
     declarations: [
-        AppComponent
+        AppContainer
     ],
     schemas: [
         NO_ERRORS_SCHEMA
---
+++ src/app/container/app/app.container.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "ns-app",
+    template: `
+        <DockLayout>
+            
+        </DockLayout>
+    `
+})
+export class AppContainer { }
