21bc8aacc7bc779d90fb9bd9c15295940498eef2
Depickere Sven <sven.depickere@student.howest.be>
2020-02-15 19:37:16 +0100

    init app container

--- src/app/container/app/app.container.ts
+++ src/app/container/app/app.container.ts
@@ -2,10 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "ns-app",
+    styleUrls: ['./app.container.scss'],
     template: `
-        <DockLayout>
-            
-        </DockLayout>
+        <Label class="test" text="hello welcome {{ title }}"></Label>
     `
 })
-export class AppContainer { }
+export class AppContainer {
+
+    title: string = 'Angular'
+}
