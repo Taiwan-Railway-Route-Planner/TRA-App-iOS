6f9247ae614b29d113f19ecdcb716779a45b6d87
Depickere Sven <sven.depickere@student.howest.be>
2020-02-16 14:37:21 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { httpLoaderFactory } from '~/app/factories/http-loader.factory';
+import { httpLoaderFactory } from './factories/http-loader.factory';
 
 import { AppComponent } from './app.component';
 import { StartUpComponent } from './container/start-up/start-up.component';
 
-import { BottomBarMenuModule } from '~/app/component/bottom-bar-menu/bottom-bar-menu.module';
+import { BottomBarMenuModule } from './component/bottom-bar-menu/bottom-bar-menu.module';
+import { AppSandbox } from './app.sandbox';
+import { LanguageService } from './service/language.service';
 
 @NgModule({
     bootstrap: [
@@ -27,11 +30,15 @@ import { BottomBarMenuModule } from '~/app/component/bottom-bar-menu/bottom-bar-
             }
         }),
         BottomBarMenuModule,
+        CommonModule,
     ],
     declarations: [
         AppComponent,
         StartUpComponent
+    ],
+    providers: [
+        AppSandbox,
+        LanguageService
     ]
 })
-export class AppModule {
-}
+export class AppModule {}
