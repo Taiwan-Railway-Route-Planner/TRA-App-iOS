c04bd7ee20be3ca86818b621d12e44e268e72df8
Depickere Sven <sven.depickere@student.howest.be>
2020-02-16 14:37:18 +0100

    Update start-up.component.ts

--- src/app/container/start-up/start-up.component.ts
+++ src/app/container/start-up/start-up.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Language } from '../../types/language.type';
+import { AppSandbox } from '../../app.sandbox';
 
 @Component({
   selector: 'ns-start-up',
   styleUrls: ['./start-up.component.scss'],
   template: `
-
+      <DockLayout>
+        <FlexboxLayout dock="top" class="main-menu">
+          <Label class="title" text="TRA Route Planner"></Label>
+        </FlexboxLayout>
+        <FlexboxLayout dock="bottom" class="menu-bottom">
+          <Button *ngIf="languageSelected" class="btn" text=""></Button>
+        </FlexboxLayout>
+        <FlexboxLayout class="menu-center">
+          <ng-container *ngFor="let item of (availableLanguages$ | async)">
+            <FlexboxLayout class="element">
+              <Label class="top-text" [text]="item.name"></Label>
+            </FlexboxLayout>
+          </ng-container>
+        </FlexboxLayout>
+      </DockLayout>
   `
 })
 export class StartUpComponent implements OnInit {
 
-  constructor() { }
+  // presentation streams
+  languageSelected = false;
+  availableLanguages$: Observable<Language[]> = this.sb.availableLanguages$;
+
+  constructor(private sb: AppSandbox) { }
+
 
   ngOnInit(): void {
   }
