a75954524b7827e2fcac2e099542493676019c7d
Depickere Sven <sven.depickere@student.howest.be>
2020-02-16 12:22:35 +0100

    init start up component

---
+++ src/app/container/start-up/start-up.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-start-up',
+  styleUrls: ['./start-up.component.scss'],
+  template: `
+
+  `
+})
+export class StartUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
