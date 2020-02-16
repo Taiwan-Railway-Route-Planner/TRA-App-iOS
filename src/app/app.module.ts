89b6098f69a451d93f8bc63b68a207101a6372a7
Depickere Sven <sven.depickere@student.howest.be>
2020-02-16 10:48:38 +0100

    init ngx translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import {NgModule, NO_ERRORS_SCHEMA} from "@angular/core";
+import {NativeScriptModule} from "nativescript-angular/nativescript.module";
+import {NativeScriptHttpModule} from "nativescript-angular/http";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HttpClient} from '@angular/common/http';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppContainer } from "./container/app/app.container";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppContainer} from "./container/app/app.container";
+import {httpLoaderFactory} from "~/app/factories/http-loader.factory";
 
 @NgModule({
     bootstrap: [
@@ -10,7 +14,15 @@ import { AppContainer } from "./container/app/app.container";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: httpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
     ],
     declarations: [
         AppContainer
@@ -19,4 +31,5 @@ import { AppContainer } from "./container/app/app.container";
         NO_ERRORS_SCHEMA
     ]
 })
-export class AppModule { }
+export class AppModule {
+}
